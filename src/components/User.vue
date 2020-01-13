c3e62280c5f10df261a0a7a6e45635b4bc994728
Eduard <eddmos86@gmail.com>
2020-01-13 14:47:13 +0300

    add VUEX

--- src/components/User.vue
+++ src/components/User.vue
@@ -2,9 +2,38 @@
     <div>
         <h1>The User Page</h1>
         <hr>
-        <input type="text">
-        <button class="btn btn-success">Save</button>
-        
-
+        <input  v-model="value"/>
+        <button
+        class="btn btn-success"
+        v-on:click = "changeInputValue">Save</button>
     </div>
 </template>
+
+<script>
+export default {
+  name: 'User',
+  mounted() {
+    if (localStorage.value) {
+      this.value = localStorage.value;
+    }
+  },
+  methods: {
+    changeInputValue(){
+      localStorage.value = this.$store.state.value
+    },
+    qwert(){
+      window.console.log(this.value)
+    }
+  },
+  computed: {
+    value:{
+      get(){
+        return this.$store.state.value
+      },
+      set(value){
+        this.$store.commit('updateValue', value)
+      }
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -2,17 +2,21 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './routes'
+import store from './store/index';
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes
+  routes,
+  mode: 'history'
 })
 
 new Vue({
-  el: '#app',
   router,
+  store,
+  // el: '#app',
+  
   render: h => h(App),
 }).$mount('#app')
